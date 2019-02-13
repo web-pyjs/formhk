26664d4d4521d818ac94d52de5f511b100531763
reyx <soufiane.nass7@gmail.com>
2019-02-13 04:33:03 +0100

    Add umd configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import resolve from "rollup-plugin-node-resolve";
 import babel from "rollup-plugin-babel";
+import replace from "rollup-plugin-replace";
+import { terser } from "rollup-plugin-terser";
 import pkg from "./package.json";
 
 export default [
@@ -7,7 +9,7 @@ export default [
   {
     input: "src/index.js",
     output: {
-      file: "lib/cjs.js",
+      file: "lib/formhk.cjs.js",
       format: "cjs",
       indent: false
     },
@@ -23,7 +25,7 @@ export default [
   {
     input: "src/index.js",
     output: {
-      file: "lib/es.js",
+      file: "lib/formhk.es.js",
       format: "es",
       indent: false
     },
@@ -32,6 +34,82 @@ export default [
       resolve(),
       babel({
         exclude: "node_modules/**" // only transpile our source code
+      })
+    ]
+  },
+  // ES for Browsers
+  {
+    input: "src/index.js",
+    output: {
+      file: "lib/formhk.es.mjs",
+      format: "es",
+      indent: false
+    },
+    external: [...Object.keys(pkg.peerDependencies || {})],
+    plugins: [
+      resolve(),
+      babel({
+        exclude: "node_modules/**" // only transpile our source code
+      }),
+      replace({
+        "process.env.NODE_ENV": JSON.stringify("production")
+      }),
+      terser({
+        compress: {
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
+          warnings: false
+        }
+      })
+    ]
+  },
+  // UMD Development
+  {
+    input: "src/index.js",
+    output: {
+      file: "umd/formhk.js",
+      format: "umd",
+      globals: pkg.peerDependencies,
+      name: "Fromhk",
+      indent: false
+    },
+    external: [...Object.keys(pkg.peerDependencies || {})],
+    plugins: [
+      resolve(),
+      babel({
+        exclude: "node_modules/**" // only transpile our source code
+      }),
+      replace({
+        "process.env.NODE_ENV": JSON.stringify("development")
+      })
+    ]
+  },
+  {
+    input: "src/index.js",
+    output: {
+      file: "umd/formhk.min.js",
+      format: "umd",
+      globals: pkg.peerDependencies,
+      name: "Fromhk",
+      indent: false
+    },
+    external: [...Object.keys(pkg.peerDependencies || {})],
+    plugins: [
+      resolve(),
+      babel({
+        exclude: "node_modules/**" // only transpile our source code
+      }),
+      replace({
+        "process.env.NODE_ENV": JSON.stringify("production")
+      }),
+      terser({
+        compress: {
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
+          warnings: false
+        }
       })
     ]
   }
